bdd8ed8c47a83a6bb518f8b02fa06f16498d3197
Muhammad Iqbal Pradipta <mhdiqbalpradipta@gmail.com>
2025-10-03 11:14:53 +0700

    penambahan token di export

--- routes/exportRoutes.js
+++ routes/exportRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const exportController = require("../controllers/master/exportController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
 
 // endpoint untuk export pendapatan.json
 router.get("/export/pendapatan", exportController.exportPendapatan);
 // endpoint untuk export penjualan.json
 router.get("/export/penjualan", exportController.exportPenjualan);
-router.get("/export/all", exportController.exportAll);
+router.get("/export/all", authMiddleware, exportController.exportAll);
 
 module.exports = router;
