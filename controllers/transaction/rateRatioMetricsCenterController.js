fafb18e994844d269fb457fbaa389b49257aba3a
Muhammad Iqbal Pradipta <mhdiqbalpradipta@192.168.1.10>
2026-04-30 09:26:13 +0700

    penambahan nilai pusat

--- controllers/transaction/rateRatioMetricsCenterController.js
+++ controllers/transaction/rateRatioMetricsCenterController.js
@@ -1,13 +1,89 @@
 const { Entities } = require("../../models");
+const { getAllDescendants } = require("../../utils/getDescendants.js");
 const {
   fetchRatesAndRatiosAggregates,
   buildRatesAndRatiosFromAggregates,
   buildRatesAndRatios,
 } = require("./rateRatioMetricsController");
 
+const RATE_KEYS = [
+  "rate_satu",
+  "rate_dua",
+  "rate_tiga",
+  "rate_empat",
+  "rate_lima",
+  "rate_enam",
+  "rate_tujuh",
+  "rate_delapan",
+  "rate_sembilan",
+  "rate_sepuluh",
+  "rate_sebelas",
+];
+
+const RATIO_KEYS = [
+  "ratio_satu",
+  "ratio_dua",
+  "ratio_tiga",
+  "ratio_empat",
+  "ratio_lima",
+  "ratio_enam",
+  "ratio_tujuh",
+  "ratio_delapan",
+  "ratio_sembilan",
+  "ratio_sepuluh",
+  "ratio_sebelas",
+];
+
 const parseInteger = (value) => {
   const parsed = parseInt(value, 10);
   return Number.isInteger(parsed) ? parsed : undefined;
+};
+
+const uniqueIntegers = (values) => [
+  ...new Set(
+    values
+      .map((value) => Number(value))
+      .filter((value) => Number.isInteger(value))
+  ),
+];
+
+const pickMetrics = (metrics, keys) =>
+  keys.reduce((result, key) => {
+    result[key] = metrics?.[key] || [];
+    return result;
+  }, {});
+
+const withRateRatioGroups = (target, metrics) => ({
+  ...target,
+  rate: pickMetrics(metrics, RATE_KEYS),
+  ratio: pickMetrics(metrics, RATIO_KEYS),
+});
+
+const buildCabangMetrics = async (cabang, yearInt, selectedMonth) => {
+  const descendants = await getAllDescendants(cabang.id);
+  const branchIds = uniqueIntegers(descendants.map((item) => item?.id));
+  const unitEntities = descendants.filter((item) => item?.type === "UNIT");
+  const unitIds = uniqueIntegers(unitEntities.map((item) => item?.id));
+
+  const metrics = await buildRatesAndRatios(
+    branchIds,
+    yearInt,
+    selectedMonth,
+    unitIds
+  );
+
+  return withRateRatioGroups(
+    {
+      cabang_id: Number(cabang.id),
+      cabang_name: cabang.name,
+      cabang_type: cabang.entity_type,
+      parent_id: cabang.parent_id,
+      included_branch_ids: branchIds,
+      included_unit_ids: unitIds,
+      unit_count: unitEntities.length,
+    },
+    metrics
+  );
 };
 
 const handleGetRatesRatiosCenter = async (req, res) => {
@@ -31,6 +107,13 @@ const handleGetRatesRatiosCenter = async (req, res) => {
       });
     }
 
+    const cabangEntities = await Entities.findAll({
+      where: { entity_type: "CABANG", is_active: true },
+      attributes: ["id", "name", "entity_type", "parent_id"],
+      order: [["id", "ASC"]],
+      raw: true,
+    });
+
     const unitEntities = await Entities.findAll({
       where: { entity_type: "UNIT", is_active: true },
       attributes: ["id", "name", "entity_type", "parent_id"],
@@ -42,25 +125,48 @@ const handleGetRatesRatiosCenter = async (req, res) => {
       .map((item) => Number(item.id))
       .filter((id) => Number.isInteger(id));
 
+    const cabangResults = [];
+    for (const cabang of cabangEntities) {
+      cabangResults.push(await buildCabangMetrics(cabang, yearInt, selectedMonth));
+    }
+
+    const centerBranchIds = cabangResults.length
+      ? uniqueIntegers(
+          cabangResults.flatMap((cabang) => cabang.included_branch_ids || [])
+        )
+      : unitIds;
+    const centerUnitIds = cabangResults.length
+      ? uniqueIntegers(
+          cabangResults.flatMap((cabang) => cabang.included_unit_ids || [])
+        )
+      : unitIds;
+
     const metrics = await buildRatesAndRatios(
-      unitIds,
+      centerBranchIds,
       yearInt,
       selectedMonth,
-      unitIds
+      centerUnitIds
     );
 
-    const response = {
-      success: true,
-      pusat_name: "Pusat",
-      pusat_type: "PUSAT",
-      year: yearInt,
-      selected_month: selectedMonth,
-      included_pusat_ids: unitIds,
-      unit_count: unitEntities.length,
-      komentar:
-        "Perhitungan PUSAT memakai semua UNIT aktif.",
-      ...metrics,
-    };
+    const response = withRateRatioGroups(
+      {
+        success: true,
+        pusat_name: "Pusat",
+        pusat_type: "PUSAT",
+        year: yearInt,
+        selected_month: selectedMonth,
+        included_pusat_ids: centerBranchIds,
+        included_cabang_ids: cabangResults.map((cabang) => cabang.cabang_id),
+        included_unit_ids: centerUnitIds,
+        cabang_count: cabangResults.length,
+        unit_count: unitEntities.length,
+        komentar:
+          "Perhitungan PUSAT memakai gabungan semua CABANG aktif beserta UNIT turunannya.",
+      },
+      metrics
+    );
+
+    response.cabangs = cabangResults;
 
     const unitAggregates = unitIds.length
       ? await fetchRatesAndRatiosAggregates(unitIds, yearInt, selectedMonth, true)
@@ -76,12 +182,14 @@ const handleGetRatesRatiosCenter = async (req, res) => {
         [unitId]
       );
 
-      return {
-        unit_id: unitId,
-        unit_name: unit.name,
-        parent_id: unit.parent_id,
-        ...unitMetrics,
-      };
+      return withRateRatioGroups(
+        {
+          unit_id: unitId,
+          unit_name: unit.name,
+          parent_id: unit.parent_id,
+        },
+        unitMetrics
+      );
     });
 
     return res.json(response);
